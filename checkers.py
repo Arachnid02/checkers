7580b29c0b0c74bfbf6a99f50267cf5ca54c0a58
Arachnid02 <jianwen.chen@uamaker.nyc>
2019-02-18 02:03:14 -0500

    Optimized efficiency of first row of squares; used variables and while loop

--- checkers.py
+++ checkers.py
@@ -2,50 +2,34 @@ from graphics import *
 
 winX = int(900)
 winY = int(900)
-blackX = int((winX / 10) / 2)
-blackY = int((winY / 10) / 2)
-redX = int((winX / 10) / 2)
-redY = int((winY / 10) / 2)
+checkerX = int(winX / 10)
+checkerY = int(winY / 10)
+rowCount = int(0)
+x1Count = int(1)
+y1Count = int(1)
+x2Count = int(2)
+y2Count = int(2)
 
 window = GraphWin("Frankie Boi's Checkerboard", winX, winY)
 window.setCoords(0, 0, 900, 900)
 
-blackChecker = Rectangle(Point(blackX * 2, blackY * 2),
-                         Point(blackX * 4, blackY * 4))
-blackChecker.setFill(color_rgb(0, 0, 0))
-blackChecker.draw(window)
+while rowCount < 4:
+    blackChecker = Rectangle(Point(checkerX * x1Count, checkerY * y1Count),
+                             Point(checkerX * x2Count, checkerY * y2Count))
+    blackChecker.setFill(color_rgb(0, 0, 0))
+    blackChecker.draw(window)
+    
+    x1Count += 1
+    x2Count += 1
 
-redChecker = Rectangle(Point(redX * 4, redY * 2),
-                       Point(redX * 6, redY * 4))
-redChecker.setFill(color_rgb(255, 0, 0))
-redChecker.draw(window)
+    redChecker = Rectangle(Point(checkerX * x1Count, checkerY * y1Count),
+                           Point(checkerX * x2Count, checkerY * y2Count))
+    redChecker.setFill(color_rgb(255, 0, 0))
+    redChecker.draw(window)
 
-blackChecker = Rectangle(Point(blackX * 6, blackY * 2),
-                         Point(blackX * 8, blackY * 4))
-blackChecker.setFill(color_rgb(0, 0, 0))
-blackChecker.draw(window)
-
-redChecker = Rectangle(Point(redX * 8, redY * 2),
-                      Point(redX * 10, redY * 4))
-redChecker.setFill(color_rgb(255, 0, 0))
-redChecker.draw(window)
-
-blackChecker = Rectangle(Point(blackX * 10, blackY * 2),
-                        Point(blackX * 12, blackY * 4))
-blackChecker.setFill(color_rgb(0, 0, 0))
-blackChecker.draw(window)
-
-redChecker = Rectangle(Point(redX * 12, redY * 2),
-                       Point(redX * 14, redY *4))
-redChecker.setFill(color_rgb(255, 0, 0))
-redChecker.draw(window)
-
-blackChecker = Rectangle(Point(blackX * 14, blackY * 2),
-                         Point(blackX * 16, blackY *4))
-blackChecker.setFill(color_rgb(0, 0, 0))
-blackChecker.draw(window)
-
-redChecker = Rectangle(Point(redX * 16, redY * 2),
-                       Point(redX * 18, redY * 4))
-redChecker.setFill(color_rgb(255, 0, 0))
-redChecker.draw(window)
+    x1Count += 1
+    x2Count += 1
+    
+    rowCount += 1
+    
+    print(rowCount)
